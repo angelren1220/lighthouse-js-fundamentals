da56d2a9b2bf06fb1e993671489637620ed15a2a
Linuxe <angelren1220@gmail.com>
2023-01-11 03:55:59 -0500

    sorted merge

--- concat-arrays.js
+++ concat-arrays.js
@@ -1,4 +1,4 @@
-const concat = function(array1, array2){
+const merge = function(array1, array2){
   let array = [];
   for(let i = 0; i < array1.length; i++){
     array.push(array1[i]);
@@ -6,10 +6,9 @@ const concat = function(array1, array2){
   for(let i = 0; i < array2.length; i++){
     array.push(array2[i]);
   } 
-  return array; 
+  return array.sort(); 
 }
 
-console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
-console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
-console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
-console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
